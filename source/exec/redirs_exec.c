d0c4412ee8696aad93a5c127b9b692b2ba65e7fa
Ana Andric <aandric@z2r7p2.42lyon.fr>
2022-03-23 13:40:57 +0100

    changed the name of files redir ; redir parse and redir exec

--- source/exec/redirs_exec.c
+++ source/exec/redirs_exec.c
@@ -6,18 +6,17 @@
 /*   By: aandric <aandric@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 17:17:40 by aandric           #+#    #+#             */
-/*   Updated: 2022/03/23 13:00:25 by aandric          ###   ########lyon.fr   */
+/*   Updated: 2022/03/23 13:36:25 by aandric          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
 t_bool	ft_redir(t_shell *shell, t_cmd *cmd)
 {
-	if (!cmd->first_redir)
+	if (!cmd->redir_first)
 		return (0);
-	if (kind == RD_NONE)
+	if (cmd->redir_first->kind == RD_NONE)
 	return (1);
 }
 
@@ -26,11 +25,13 @@ t_bool ft_redir_out(t_shell *shell, t_cmd *cmd)
 	int	fd_in;
 	int	fd_out;
 
-	fd_in = open(cmd->first_redir->filename, O_RDONLY);
+	fd_in = openopen(cmd->redir_first->filename, O_RDONLY);
 	if (fd_in < 0)
 		return (0);
-	fd_out = open(cmd->last_redir->filename, O_WRONLY);
+	fd_out = open(cmd->redir_last->filename, O_RDONLY);
 	if (fd_out < 0)
 		return (0);
 	dup2(fd_in, fd_out);
 }
+
+
--- source/parse/parse.h
+++ source/parse/parse.h
@@ -3,45 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   parse.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: soumanso <soumanso@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: aandric <aandric@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD:redir.c
-/*   Created: 2022/03/18 17:17:40 by aandric           #+#    #+#             */
-/*   Updated: 2022/03/18 19:23:04 by aandric          ###   ########lyon.fr   */
-=======
 /*   Created: 2022/03/21 15:08:09 by soumanso          #+#    #+#             */
-/*   Updated: 2022/03/21 15:37:34 by soumanso         ###   ########lyon.fr   */
->>>>>>> 82259dd9363502f33ccbecd0723302994e61fc55:source/parse/parse.h
+/*   Updated: 2022/03/23 13:23:45 by aandric          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSE_H
 # define PARSE_H
 
-<<<<<<< HEAD:redir.c
-
-t_bool	ft_redir(t_shell *shell, t_cmd *cmd)
-{
-	if (!cmd->first_redir)
-		return (0);
-	if (kind == RD_NONE)
-	return (1);
-}
-
-t_bool ft_redir_out(t_shell *shell, t_cmd *cmd)
-{
-	int	fd_in;
-	int	fd_out;
-
-	fd_in = open(cmd->first_redir->filename, O_RDONLY);
-	if (fd_in < 0)
-		return (0);
-	fd_out = open(cmd->last_redir->filename, O_WRONLY);
-	if (fd_out < 0)
-		return (0);
-	dup2(fd_in, fd_out);
-}
-=======
 # include "minishell.h"
 
 t_str			post_process_token(t_shell *sh, t_cstr str, t_int len);
@@ -51,4 +22,3 @@ t_bool			cmd_add_arg(t_cmd *cmd, t_token *token);
 t_bool			cmd_null_terminate_args(t_cmd *cmd);
 
 #endif
->>>>>>> 82259dd9363502f33ccbecd0723302994e61fc55:source/parse/parse.h
