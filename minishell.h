10e98bdec850e26a4898f4080f5bad9b228f4e19
ostef <47391841+ostef@users.noreply.github.com>
2022-03-29 13:52:36 +0200

    Added missing #includes.

--- minishell.h
+++ minishell.h
@@ -16,7 +16,12 @@
 # include "libft.h"
 # include <readline/readline.h>
 # include <readline/history.h>
-
+# include <limits.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
 
 typedef enum e_err
 {
