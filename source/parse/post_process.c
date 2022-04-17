8f548a9d626dee47b506e6f950351d3ef243c937
ostef <47391841+ostef@users.noreply.github.com>
2022-04-17 17:26:17 +0200

    Rewrote handling of quotes in parsing, to allow a='b c d' situation (which should result in 'a=b c d', not 'a=' 'b c d').

--- source/parse/post_process.c
+++ source/parse/post_process.c
@@ -6,7 +6,7 @@
 /*   By: soumanso <soumanso@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 15:07:04 by soumanso          #+#    #+#             */
-/*   Updated: 2022/04/07 15:37:38 by soumanso         ###   ########lyon.fr   */
+/*   Updated: 2022/04/17 17:04:35 by soumanso         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,42 @@
 
 t_str	expand_variables(t_shell *sh, t_cstr str, t_int len)
 {
-	t_lexer	lexer;
-	t_token	*token;
-	t_str	result;
+	t_lexer		lexer;
+	t_token		*token;
+	t_builder	builder;
+	char		quote;
 
+	ft_builder_init (&builder, 100, ft_temp ());
 	ft_lexer_init_n(&lexer, str, len, ft_temp());
-	result = "";
+	quote = 0;
 	while (lexer.curr < lexer.end)
 	{
-		token = ft_lexer_skip_delim(&lexer, "$");
-		result = ft_fmt(ft_temp(), "%s%.*s", result, token->len, token->str);
-		ft_lexer_skip_char(&lexer, '$');
-		token = ft_lexer_skip_char(&lexer, '?');
-		if (token)
-			result = ft_fmt(ft_temp(), "%s%d", result, g_globals.exit_status);
+		if (ft_lexer_skip_char (&lexer, '\'') || ft_lexer_skip_char (&lexer, '"'))
+		{
+			if (quote == *(lexer.curr - 1))
+				quote = 0;
+			else if (quote == 0)
+				quote = *(lexer.curr - 1);
+			continue ;
+		}
+		if (quote != '\'' && ft_lexer_skip_char (&lexer, '$'))
+		{
+			token = ft_lexer_skip_identifier (&lexer);
+			if (token)
+				ft_builder_append (&builder, env_get (sh,
+							ft_strndup (token->str, token->len, ft_temp ())));
+			else if (ft_lexer_skip_char (&lexer, '?'))
+				ft_builder_append_fmt (&builder, "%d", g_globals.exit_status);
+			else
+				ft_builder_append_char (&builder, '$');
+		}
 		else
 		{
-			token = ft_lexer_skip_identifier(&lexer);
-			if (token)
-				result = ft_fmt(ft_temp(), "%s%s", result, env_get(sh,
-							ft_strndup(token->str, token->len, ft_temp())));
+			ft_builder_append_char (&builder, *lexer.curr);
+			lexer.curr += 1;
 		}
 	}
-	return (result);
+	return (ft_builder_build (&builder));
 }
 
 t_str	post_process_token(t_shell *sh, t_cstr str, t_int len)
