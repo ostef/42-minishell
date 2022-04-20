b4937207f05b4ccb8589f212d9f99f5ddd2798a1
Ana Andric <aandric@z2r12p2.42lyon.fr>
2022-04-20 17:35:55 +0200

    handled problem with different type of quotes inside

--- source/parse/post_process.c
+++ source/parse/post_process.c
@@ -3,19 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   post_process.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: soumanso <soumanso@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: aandric <aandric@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 15:07:04 by soumanso          #+#    #+#             */
-/*   Updated: 2022/04/17 17:04:35 by soumanso         ###   ########lyon.fr   */
+/*   Updated: 2022/04/20 17:04:50 by aandric          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 
+static void	handle_dollars(t_shell *sh, t_lexer *lexer, t_builder *builder)
+{
+	t_token	*token;
+
+	token = ft_lexer_skip_identifier (lexer);
+	if (token)
+		ft_builder_append (builder, env_get (sh,
+				ft_strndup (token->str, token->len, ft_temp ())));
+	else if (ft_lexer_skip_char (lexer, '?'))
+		ft_builder_append_fmt (builder, "%d", g_globals.exit_status);
+	else
+		ft_builder_append_char (builder, '$');
+}
+
+static t_bool	handle_quote(t_lexer *lexer, t_builder *builder, char *quote)
+{
+	if (ft_lexer_skip_char(lexer, '\'') || ft_lexer_skip_char (lexer, '"'))
+	{
+		if (*quote == *(lexer->curr - 1))
+			*quote = 0;
+		else if (*quote == 0)
+			*quote = *(lexer->curr - 1);
+		else
+			ft_builder_append_char (builder, *(lexer->curr - 1));
+		return (TRUE);
+	}
+	return (FALSE);
+}
+
 t_str	expand_variables(t_shell *sh, t_cstr str, t_int len)
 {
 	t_lexer		lexer;
-	t_token		*token;
 	t_builder	builder;
 	char		quote;
 
@@ -24,25 +52,10 @@ t_str	expand_variables(t_shell *sh, t_cstr str, t_int len)
 	quote = 0;
 	while (lexer.curr < lexer.end)
 	{
-		if (ft_lexer_skip_char (&lexer, '\'') || ft_lexer_skip_char (&lexer, '"'))
-		{
-			if (quote == *(lexer.curr - 1))
-				quote = 0;
-			else if (quote == 0)
-				quote = *(lexer.curr - 1);
+		if (handle_quote(&lexer, &builder, &quote))
 			continue ;
-		}
 		if (quote != '\'' && ft_lexer_skip_char (&lexer, '$'))
-		{
-			token = ft_lexer_skip_identifier (&lexer);
-			if (token)
-				ft_builder_append (&builder, env_get (sh,
-							ft_strndup (token->str, token->len, ft_temp ())));
-			else if (ft_lexer_skip_char (&lexer, '?'))
-				ft_builder_append_fmt (&builder, "%d", g_globals.exit_status);
-			else
-				ft_builder_append_char (&builder, '$');
-		}
+			handle_dollars(sh, &lexer, &builder);
 		else
 		{
 			ft_builder_append_char (&builder, *lexer.curr);
